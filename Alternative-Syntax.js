c8780264e7668192a27207386aa9129a793698d7
Aymane Mehdi <unlocked.dark.web@gmail.com>
2023-08-23 14:10:13 +0100

    Alternative Syntax

---
+++ Alternative-Syntax.js
@@ -0,0 +1,9 @@
+module.exports.items = ['item1','item2']
+const person ={
+    name:'aymane',
+}
+
+module.exports.person = person
+
+// const items = ['item1','item2']
+// const person ={name:'aymane',}
---
+++ app.js
@@ -0,0 +1,3 @@
+const data = require ('./Alternative-Syntax')
+
+console.log(data)
